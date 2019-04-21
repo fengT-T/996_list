6866710a4e520981da5be7bf6aa855d6860019c9
leon <cwljx@icloud.com>
2019-04-21 14:31:57 +0800

    搜索功能

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,20 +1,25 @@
 <template>
-  <div class="list">
-    <div class="card box-shadow" v-for="(item, index) in list" :key="index">
-      <div class="card-header">
-        <div class="card-title h5">{{item.name}}</div>
-        <div class="card-subtitle text-gray">{{item.postion}}	</div>
+  <div class="main">
+    <input type="text" v-model="value" class="search" placeholder="Search here...">
+    <div class="list">
+      <div class="card box-shadow" v-for="(item, index) in list" :key="index" :style="isShow(item)">
+        <div>
+          <div class="card-header">
+            <div class="card-title h5">{{item.name}}</div>
+            <div class="card-subtitle text-gray">{{item.postion}}	</div>
+          </div>
+          <div class="card-body">
+            <p>制度描述： {{ item.rule}}</p>
+            <p v-html="item.evidence"></p>
+          </div>
+          <div class="card-footer">
+            <a class="c-hand" target="blank" :href="`https://utteranc.es/utterances.html?src=https://utteranc.es/client.js&repo=fengT-T/996_comment&issue-term=${item.name}&crossorigin=anonymous&theme=github-light&async=&url=https://fengt-t.github.io/996_list/&origin=https://fengt-t.github.io/996_list/&pathname=996_list&title=996List&description=${item.name}&og:title=996List`">评论</a>
+            <span class="text-gray">{{item.time}}</span>
+          </div>
+        </div>
       </div>
-      <div class="card-body">
-        <p>制度描述： {{ item.rule}}</p>
-        <p v-html="item.evidence"></p>
-      </div>
-      <div class="card-footer">
-        <a class="c-hand" target="blank" :href="`https://utteranc.es/utterances.html?src=https://utteranc.es/client.js&repo=fengT-T/996_comment&issue-term=${item.name}&crossorigin=anonymous&theme=github-light&async=&url=https://fengt-t.github.io/996_list/&origin=https://fengt-t.github.io/996_list/&pathname=996_list&title=996List&description=${item.name}&og:title=996List`">评论</a>
-        <span class="text-gray">{{item.time}}</span>
-      </div>
+      <div v-show="loading" class="loading loading-lg"></div>
     </div>
-    <div v-show="loading" class="loading loading-lg"></div>
   </div>
 </template>
 <script>
@@ -26,13 +31,20 @@ export default {
     url: {
       type: String,
       required: true,
-    }
+    },
   },
   data () {
     return {
       list: [],
       progess: 0,
-      loading: true
+      loading: true,
+      value: null,
+    }
+  },
+  methods: {
+    isShow(item) {
+      if (this.value)
+      return item.name.includes(this.value)? '' : 'display: none'
     }
   },
   filters: {
@@ -49,10 +61,27 @@ export default {
 }
 </script>
 <style lang="less">
+.main{
+  text-align: center;
+  .search{
+    width: 300px;
+    display: inline-block;
+    border: none;
+    border-bottom: .05rem solid #dadee4;
+    outline: none;
+    background: 270px center / 20px 20px url(../assets/search.svg) no-repeat;
+    box-shadow: #5755d9;
+  }
+  .search:focus{
+    border-bottom-color: #5755d9;
+    color: #5755d9;
+  }
+}
 .list {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
+  
   .card {
     width: 300px;
     border: none;
